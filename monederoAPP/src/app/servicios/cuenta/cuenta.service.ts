429ab5cc6640bd4ad8e3ce96550675383980b9bc
Ovidio Rodriguez Ferretti <ovifer24@hotmail.com>
2020-06-29 18:41:09 -0400

    serivico cuenta

---
+++ monederoAPP/src/app/servicios/cuenta/cuenta.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CuentaService {
+
+    constructor(private http: HttpClient, private form: FormBuilder) { }
+
+
+
+    obtenerTipoCuenta(){
+        let header = new HttpHeaders({'Authorization': 'Bearer ' + localStorage.getItem('token')});
+        return this.http.get('http://localhost:5000/api/Dashboard/TiposCuentas', {headers: header});
+    }
+
+    obtenerBanco(){
+        let header = new HttpHeaders({'Authorization': 'Bearer ' + localStorage.getItem('token')});
+        return this.http.get('http://localhost:5000/api/Dashboard/Bancos', {headers: header});
+    }
+
+
+    obtenerCuentas(){
+        let header = new HttpHeaders({'Authorization': 'Bearer ' + localStorage.getItem('token')});
+        let param = new HttpParams().set('idUsuario', localStorage.getItem('idUsuario'));
+
+        return this.http.get('http://localhost:5000/api/Dashboard/cuentas',{params: param, headers: header})
+    }
+
+}
