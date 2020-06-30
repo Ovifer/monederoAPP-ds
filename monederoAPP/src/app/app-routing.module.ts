b6399581025a932577799bc8dc66aabb9f0fe375
Ovidio Rodriguez Ferretti <ovifer24@hotmail.com>
2020-06-29 21:23:32 -0400

    Modificacion de App

--- monederoAPP/src/app/app-routing.module.ts
+++ monederoAPP/src/app/app-routing.module.ts
@@ -3,14 +3,50 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'monedero',
-    loadChildren: () => import('./monedero/home.module').then(m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'monedero',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
+  {
+    path: 'monedero',
+    loadChildren: () => import('./monedero/monedero.module').then( m => m.PostPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'solicitar-pago',
+    loadChildren: () => import('./monedero/solicitarPago/solicitarPago.module').then( m => m.SolicitarPagoPageModule)
+  },
+  {
+    path: 'aprobar-reintegro',
+    loadChildren: () => import('./monedero/aprobarReintegro/aprobarReintegro.module').then( m => m.AprobarReintegroPageModule)
+  },
+  {
+    path: 'perfil',
+    loadChildren: () => import('./monedero/perfil/perfil.module').then( m => m.PerfilPageModule)
+  },
+  {
+    path: 'registro',
+    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
+  },
+  {
+    path: 'billetera',
+    loadChildren: () => import('./monedero/billetera/billetera.module').then( m => m.BilleteraPageModule)
+  },
+  {
+    path: 'cobros',
+    loadChildren: () => import('./monedero/cobros/cobros.module').then( m => m.CobrosPageModule)
+  },
+  {
+    path: 'tarjeta-reintegro',
+    loadChildren: () => import('./monedero/tarjetaReintegro/tarjetaReintegro.module').then( m => m.TarjetaReintegroPageModule)
+  },
+  {
+    path: 'cuenta-reintegro',
+    loadChildren: () => import('./monedero/cuentaReintegro/cuentaReintegro.module').then( m => m.CuentaReintegroPageModule)
+  }
 ];
 
 @NgModule({
--- monederoAPP/src/app/app.component.ts
+++ monederoAPP/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -11,9 +10,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 })
 export class AppComponent {
   constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+      private platform: Platform,
+      private splashScreen: SplashScreen,
+      private statusBar: StatusBar
   ) {
     this.initializeApp();
   }
